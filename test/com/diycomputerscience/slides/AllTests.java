a512f67b98ef4d0c1fe7d29d280101f8f20e4250
Parag Shah <adaptives@gmail.com>
2012-04-17 12:36:05 +0530

    added AllTests

---
+++ test/com/diycomputerscience/slides/AllTests.java
@@ -0,0 +1,24 @@
+package com.diycomputerscience.slides;
+
+import com.diycomputerscience.slides.service.SlideServiceTest;
+import com.diycomputerscience.slides.util.ModelTOConverterTest;
+import com.diycomputerscience.slides.view.action.TestHomeAction;
+import com.diycomputerscience.slides.view.action.TestSlideShowAction;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		//$JUnit-BEGIN$
+		suite.addTestSuite(SlideServiceTest.class);
+		suite.addTestSuite(ModelTOConverterTest.class);
+//		suite.addTestSuite(TestHomeAction.class);
+//		suite.addTestSuite(TestSlideShowAction.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
