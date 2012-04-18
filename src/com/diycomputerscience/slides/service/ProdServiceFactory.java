e4f0458291949a0d0d9fef8a11caf9c9a34e12b0
Parag Shah <adaptives@gmail.com>
2012-04-18 12:18:36 +0530

    changed the JNDI name for SlideService

--- src/com/diycomputerscience/slides/service/ProdServiceFactory.java
+++ src/com/diycomputerscience/slides/service/ProdServiceFactory.java
@@ -16,7 +16,7 @@ public class ProdServiceFactory implements IServiceFactory {
 	public SlideService getSlideService() throws ServiceLookupException {
 		
 		try {
-			String jndiName = "java:global/slides/SlideService"; //"java:module/SlideService";
+			String jndiName = "java:module/SlideService";
 			return (SlideService)new InitialContext().lookup(jndiName);
 		} catch (NamingException e) {
 			String msg = "Could not get SlideService bean";
